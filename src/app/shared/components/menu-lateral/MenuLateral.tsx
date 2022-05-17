5ec1cc96998a8f191c9071a70c1907c5e45e1e47
Augusto Souza <augusto_souza@edu.unifil.br>
2022-05-17 14:36:25 -0300

    component avatar

--- src/app/shared/components/menu-lateral/MenuLateral.tsx
+++ src/app/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,4 +1,5 @@
-import { Box, Drawer, useTheme } from "@mui/material";
+import { Avatar, Box, Divider, Drawer, useTheme } from "@mui/material";
+import { height } from "@mui/system";
 import { ReactNode } from "react";
 
 interface IMenuLateral {
@@ -9,7 +10,29 @@ export const MenuLateral: React.FC<IMenuLateral> = ({ children }) => {
   return (
     <>
       <Drawer variant={"permanent"}>
-        <Box width={theme.spacing(28)}>Teste</Box>
+        <Box
+          width={theme.spacing(28)}
+          height={"100%"}
+          display={"flex"}
+          flexDirection={"column"}
+        >
+          <Box
+            width={"100%"}
+            height={theme.spacing(20)}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Avatar
+              sx={{
+                height: theme.spacing(10),
+                width: theme.spacing(10),
+              }}
+            />
+          </Box>
+          <Divider />
+          <Box flex={1}></Box>
+        </Box>
       </Drawer>
       <Box height={"100vh"} marginLeft={theme.spacing(28)}>
         {children}
