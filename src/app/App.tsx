7b542612e707552e8734afa48212c9675f2738c8
Augusto Souza <augusto_souza@edu.unifil.br>
2022-05-17 11:10:33 -0300

    add rotas react -router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
+import { AppRoutes } from "./routes";
+
 const App = () => {
   return (
     <div>
       <h1>React JS + MUI</h1>
+      <AppRoutes />
     </div>
   );
-}
+};
 
-export {App};
+export { App };
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,1 +1,13 @@
-export {};
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "../pages";
+
+export const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/pagina-inicial"} element={<Home />} />
+        <Route path={"*"} element={<Navigate to={"/pagina-inicial"} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
